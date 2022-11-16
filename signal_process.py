115b59e06e9df360da6f1d2e6077d3cc8f43f5d1
emanuelthiago06 <96442154+emanuelthiago06@users.noreply.github.com>
2022-11-16 08:51:59 -0300

    Add files via upload

--- signal_process.py
+++ signal_process.py
@@ -12,78 +12,77 @@ from timeit import default_timer as timer
 start = timer()
 total_signal = 1
 
-regex_signal_name = r"^Results_alt/(\w+)/(?P<numero>\d+_hr)"
+regex_signal_name = r"^Results_alt2/(\w+)/(?P<classe>\d)/(?P<numero>\d+_hr)/(?P<ciclo>\d+)"
+regex_dir_stop = r"^(?P<numero>\d+_hr)"
 cont =0
 contSignal = 1
+dir_exe = ['0','1']
 
 loops = ["val","training","test"]
-
+n = 0
 for j in loops:
-    dados = pd.DataFrame(columns=('signalName', 'cycle',
+    dados = pd.DataFrame(columns=('signalName', 'cycle','classe','split',
                               'd1_X', 'd2_X', 'd_dif_X', 'sim_X', 'd_pond_X',
                               'd1_Y', 'd2_Y', 'd_dif_Y', 'sim_Y', 'd_pond_Y',
                               'd1_Z', 'd2_Z', 'd_dif_Z', 'sim_Z', 'd_pond_Z'))
-    for root,dir,files in os.walk(f"Results_alt/{j}",topdown=False):
-        if dir:
-            print(dir)
-            break
-        total_signal+=1
+    for root,dir,files in os.walk(f"Results_alt2/{j}",topdown=False):
+        try:
+            tr = root+'/'+files[1]
+            total_signal+=1
+        except:
+            tr = 0
     print(total_signal)
-
-    for root,dir,files in os.walk(f"Results_alt/{j}",topdown=False):
-        if dir:
-            break
-        match = re.search(regex_signal_name,root)
-        signalName = match.group("numero")
-        xt1 = root+'/'+files[1]
-        yt1 = root+'/'+files[3]
-        zt1 = root+'/'+files[4]
-        xt2 = root+'/'+files[2]
-        yt2 = root+'/'+files[5]
-        zt2 = root+'/'+files[0]
-        # PARAMÊTROS - DERIVAÇÃO x
-        m_fixo_X = met(xt1, xt2, block_size=20)
-        cm_fixo1_X = m_fixo_X.count_method(m_fixo_X.image1)
-        cm_fixo2_X = m_fixo_X.count_method(m_fixo_X.image2)
-        dif_fixo_X = m_fixo_X.dif_method()
-        sim_fixo_X = m_fixo_X.sim_method()
-        d_pond_fixo_X = m_fixo_X.pond_method()
-
-        # PARAMETROS - DERIVACAO Y
-        m_fixo_Y = met(yt1, yt2, block_size=20)
-        cm_fixo1_Y = m_fixo_Y.count_method(m_fixo_Y.image1)
-        cm_fixo2_Y = m_fixo_Y.count_method(m_fixo_Y.image2)
-        dif_fixo_Y = m_fixo_Y.dif_method()
-        sim_fixo_Y = m_fixo_Y.sim_method()
-        d_pond_fixo_Y = m_fixo_Y.pond_method()
-
-        # PARAMETROS - DERIVACAO Z
-        m_fixo_Z = met(zt1, zt2, block_size=20)
-        cm_fixo1_Z = m_fixo_Z.count_method(m_fixo_Z.image1)
-        cm_fixo2_Z = m_fixo_Z.count_method(m_fixo_Z.image2)
-        dif_fixo_Z = m_fixo_Z.dif_method()
-        sim_fixo_Z = m_fixo_Z.sim_method()
-        d_pond_fixo_Z = m_fixo_Z.pond_method()
-
-        cycle = "not implemented, fix later"
-
-        dados.loc[cont] = [signalName, cycle, cm_fixo1_X, cm_fixo2_X, dif_fixo_X, sim_fixo_X, d_pond_fixo_X,
-                           cm_fixo1_Y, cm_fixo2_Y, dif_fixo_Y, sim_fixo_Y, d_pond_fixo_Y,
-                           cm_fixo1_Z, cm_fixo2_Z, dif_fixo_Z, sim_fixo_Z, d_pond_fixo_Z]    
-
-        cont = cont + 1
-        contSignal+=1
-
-        print(f'Signal processed: {contSignal}/{total_signal}\n')
-
-        # Pra teste apenas para limitar a quantidade de sinais processados
-
+    for root,dir,files in os.walk(f"Results_alt2/{j}",topdown=False):
+        try:
+            if files == [" "]:
+                break     
+            match = re.search(regex_signal_name,root)
+            signalName = match.group("numero")
+            cycle = match.group("ciclo")
+            classe = match.group("classe")
+            xt1 = root+'/'+files[1]
+            yt1 = root+'/'+files[3]
+            zt1 = root+'/'+files[4]
+            xt2 = root+'/'+files[2]
+            yt2 = root+'/'+files[5]
+            zt2 = root+'/'+files[0]
+             #PARAMÊTROS - DERIVAÇÃO x
+            m_fixo_X = met(xt1, xt2, block_size=20)
+            cm_fixo1_X = m_fixo_X.count_method(m_fixo_X.image1)
+            cm_fixo2_X = m_fixo_X.count_method(m_fixo_X.image2)
+            dif_fixo_X = m_fixo_X.dif_method()
+            sim_fixo_X = m_fixo_X.sim_method()
+            d_pond_fixo_X = m_fixo_X.pond_method()
+             #PARAMETROS - DERIVACAO Y
+            m_fixo_Y = met(yt1, yt2, block_size=20)
+            cm_fixo1_Y = m_fixo_Y.count_method(m_fixo_Y.image1)
+            cm_fixo2_Y = m_fixo_Y.count_method(m_fixo_Y.image2)
+            dif_fixo_Y = m_fixo_Y.dif_method()
+            sim_fixo_Y = m_fixo_Y.sim_method()
+            d_pond_fixo_Y = m_fixo_Y.pond_method()
+             #PARAMETROS - DERIVACAO Z
+            m_fixo_Z = met(zt1, zt2, block_size=20)
+            cm_fixo1_Z = m_fixo_Z.count_method(m_fixo_Z.image1)
+            cm_fixo2_Z = m_fixo_Z.count_method(m_fixo_Z.image2)
+            dif_fixo_Z = m_fixo_Z.dif_method()
+            sim_fixo_Z = m_fixo_Z.sim_method()
+            d_pond_fixo_Z = m_fixo_Z.pond_method()
+            dados.loc[cont] = [signalName, cycle, classe, j, cm_fixo1_X, cm_fixo2_X, dif_fixo_X, sim_fixo_X, d_pond_fixo_X,
+                              cm_fixo1_Y, cm_fixo2_Y, dif_fixo_Y, sim_fixo_Y, d_pond_fixo_Y,
+                              cm_fixo1_Z, cm_fixo2_Z, dif_fixo_Z, sim_fixo_Z, d_pond_fixo_Z]    
+            cont = cont + 1
+            contSignal+=1
+            print(f'Signal processed: {contSignal}/{total_signal}\n')
+            # Pra teste apenas para limitar a quantidade de sinais processados
+        except:
+            n+=1
     cont = 0
     contSignal = 0
     total_signal = 0
     dados.to_csv(f'dados_{j}_ptb.csv', mode='w')
     del dados
-# Finalização do timer
+# Finalização do 
+print(n)
 end = timer()
 etime = end - start
 etime_h = etime//3600
